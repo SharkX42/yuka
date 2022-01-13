baa07e1bdeec4bd48c12162ddf850ed74d03ff34
Nicolas MURILLON <nicolas.murillon@etu.uca.fr>
2022-01-13 15:19:31 +0100

    Added Nutriscore

--- src/main/java/com/delmur/javapro/yuka/models/Product.java
+++ src/main/java/com/delmur/javapro/yuka/models/Product.java
@@ -16,16 +16,21 @@ public class Product {
     private int NutriScore;
     private String Classe;
     private String Color;
-    private double CarboHydrates100g;
-    private double Energy100g;
-    private double Sugars100g;
-    private double Salt100g;
+//    private double CarboHydrates100g;
+//    private double Energy100g;
+//    private double Sugars100g;
+//    private double Salt100g;
 
     public Product() {
 
     }
     public Product(JsonNode product)
     {
+        double SaturatedFat100g;
+        double Energy100g;
+        double Sugars100g;
+        double Salt100g;
+
         Id = product.path("code").asInt();
         BarCode = product.path("code").asText();
 
@@ -41,10 +46,17 @@ public class Product {
 
         JsonNode nutriments = product.findPath("nutriments");
 
-        CarboHydrates100g = nutriments.path("carbohydrates_100g").asDouble();
+        SaturatedFat100g = nutriments.path("saturated-fat_100g").asDouble();
         Energy100g = nutriments.path("energy_100g").asDouble();
         Sugars100g = nutriments.path("sugars_100g").asDouble();
         Salt100g = nutriments.path("salt_100g").asDouble();
 
+        /* TODO : Rework for taking db value into account */
+        NutriScore += Math.min((int)(Energy100g / 335), 10);
+        NutriScore += Math.min((int)SaturatedFat100g,10);
+        NutriScore += Math.min((int)(Sugars100g / 4.5), 10);
+        NutriScore += Math.min((int)(Salt100g / 90), 10);
+        NutriScore -= Math.min((int)(nutriments.path("fiber_100g").asDouble()  / 0.9),5);
+        NutriScore -= Math.min((int)(nutriments.path("proteins_100g").asDouble()  / 1.6),5);
     }
 }
